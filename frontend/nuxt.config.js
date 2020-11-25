67989714ced449bf5128b2a1a350c1bf7ef67c2f
Robert <treboryx@gmail.com>
2020-11-25 21:26:10 +0200

    nvm this isn't needed

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -33,11 +33,7 @@ export default {
 
   server: {
     port: 3000, // default: 3000
-    host: "0.0.0.0", // default: localhost
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./../server.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./../server.crt"))
-    }
+    host: "0.0.0.0" // default: localhost
   },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
