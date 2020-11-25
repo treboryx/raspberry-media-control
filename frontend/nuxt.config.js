727297ff52db4d7947fdf19b03a625931545000e
Robert <treboryx@gmail.com>
2020-11-25 20:49:39 +0200

    server config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -28,6 +28,11 @@ export default {
     "@nuxtjs/tailwindcss"
   ],
 
+  server: {
+    port: 3000, // default: 3000
+    host: "0.0.0.0" // default: localhost
+  },
+
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [],
 
